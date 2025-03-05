1cae46bd5e9907fab2b0aa1283b8e133db8f08eb
cdobbi <dob24001@byui.edu>
2025-03-05 07:13:42 -0700

    object basics

---
+++ basics.js
@@ -0,0 +1,10 @@
+let bandInfo;
+
+// Put your code here
+
+
+// Don't edit the code below here
+
+let para1 = document.createElement('p');
+para1.textContent = bandInfo;
+section.appendChild(para1);
