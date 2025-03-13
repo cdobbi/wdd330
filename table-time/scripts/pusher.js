26b5c180f77a2d1dbfe93a422a6548f6bdaaaeed
cdobbi <dob24001@byui.edu>
2025-03-12 21:36:40 -0600

    Add Pusher integration for real-time notifications; implement logging and event handling

---
+++ table-time/scripts/pusher.js
@@ -0,0 +1,12 @@
+
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  var pusher = new Pusher('999673f7c045421210be', {
+    cluster: 'us3'
+  });
+
+  var channel = pusher.subscribe('my-channel');
+  channel.bind('my-event', function(data) {
+    alert(JSON.stringify(data));
+  });
