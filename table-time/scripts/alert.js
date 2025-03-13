901f4f7d0f0f5c3aab594a9855974b305147bcda
cdobbi <dob24001@byui.edu>
2025-03-13 09:05:45 -0600

    Add initial files for various dependencies and configurations

--- table-time/scripts/alert.js
+++ table-time/scripts/alert.js
@@ -38,4 +38,43 @@ document.addEventListener("DOMContentLoaded", function () {
       sendAlert(breed);
     }
   });
+
+  // Enable Pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  var pusher = new Pusher("999673f7c045421210be", {
+    cluster: "us3",
+  });
+
+  var channel = pusher.subscribe("my-channel");
+  channel.bind("my-event", function (data) {
+    // Check if the browser supports notifications and service workers
+    if ("Notification" in window && "serviceWorker" in navigator) {
+      // Request permission to display notifications
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          // Subscribe to push notifications
+          navigator.serviceWorker.ready.then(function (registration) {
+            registration.pushManager
+              .subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: "BOof9myOTsT1d4hX0gUOCizYg93nwztm_4cd5U1cBBOW2x0dRxFym9qjSwSiDSUiRuMncym9qjSwSiDSUiRuMncM4sqEfNEB7vhXNV-tw", // Replace with your actual VAPID public key
+              })
+              .then(function (subscription) {
+                console.log("Subscribed to push notifications:", subscription);
+              })
+              .catch(function (error) {
+                console.error(
+                  "Failed to subscribe to push notifications:",
+                  error
+                );
+              });
+          });
+        }
+      });
+    } else {
+      // Fallback for browsers that do not support notifications
+      alert(data.message);
+    }
+  });
 });
